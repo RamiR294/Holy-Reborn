b44be1e166e84a98517e882fbd503732d9a954ee
Ramchik <rrramirrr@yandex.ru>
2021-11-14 23:13:05 +0400

    Fonts Updated

---
+++ hud/client/cl_font.lua
@@ -0,0 +1,23 @@
+surface.CreateFont("nick" {
+	font = "AmazObitaemOstrovFine",
+	extended = true,
+	shadow = false,
+	size = 34,
+	wight = 500,
+} )
+
+surface.CreateFont("nomer" {
+	font = "AmazObitaemOstrovFine",
+	extended = true,
+	shadow = true,
+	size = 35,
+	wight = 500,
+} )
+
+surface.CreateFont("rabota" {
+	font = "AmazObitaemOstrovFine",
+	extended = true,
+	shadow = false,
+	size = 26,
+	wight = 500,
+} )
